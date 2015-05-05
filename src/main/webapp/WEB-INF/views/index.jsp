9bc1a48a073264bdc745a2b5ff9b646ef1bd3e27
abel <abel_ujs@163.com>
2015-05-05 22:52:02 +0800

    add bootstrap and mysql database

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,7 +1,11 @@
 <%@page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
 <html>
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 <body>
 	<h2>abel karen Hello World!</h2>
 	test1 test2 test3 test4 test 5 test6 测试
 </body>
+<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </html>
--- src/test/java/com/abel/HibernateTest.java
+++ src/test/java/com/abel/HibernateTest.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -18,6 +19,7 @@ import com.abel.entity.Account;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring.xml")
 @TransactionConfiguration(defaultRollback = true)
+@ActiveProfiles("test")
 public class HibernateTest {
 	private static final Logger logger = Logger.getLogger(HibernateTest.class);
     @Autowired
